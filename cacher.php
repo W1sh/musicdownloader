cc7f644d73c946782e0bbeec5b58807b1cb13ccf
W1sh <brunoferreira.ebm@gmail.com>
2018-12-03 16:58:39 +0000

    changes to class Cacher

--- cacher.php
+++ cacher.php
@@ -1,37 +1,62 @@
 <?php
-$logger = new Logger();
-
-function store($key, $data){
-    //$logger->info('Call to function store with $key->'.$key.' and $data->'.$data);
-    // store information in file
-    $json = read();
-    $json[$key] = $data;
-    file_put_contents($file, json_encode($json));
-}
-
-function fetch($key){
-    //$logger->info('Call to function retrieve with $key->'.$key);
-    // retrieve information from file
-    $json = read();
-    print_r($json[$key]);
-    return $json[$key];
-}
-
-function read(){
-    //$logger->info('Call to function read');
-    // read the configuration file
-    $file = "config.json";
-    if(is_file($file)){
-        $json = json_decode(file_get_contents($file), true);
-        return $json;
-    }else{
-        initialize(this());
+include 'logger.php';
+class Cacher {
+    private $logger;
+    public function __construct(){
+        $this->$logger = new Logger("cacher");
+    }
+    /*
+    **  Function to store information in the configuration file
+    **  @param $key -> parameter to search in the file
+    **  @param $data -> value to replace/store in the file 
+    **  @return -> none
+    */
+    function store($key, $data) {
+        $this->$logger->info('Call to function store with $key->'.$key.' and $data->'.$data);
+        $json = read();
+        $json[$key] = $data;
+        file_put_contents($file, json_encode($json));
+    }
+    /*
+    **  Function to retrieve information from the configuration file
+    **  @param $key -> parameter to search in the file
+    **  @return -> value corresponding to the key
+    */
+    function fetch($key) {
+        $this->$logger->info('Call to function retrieve with $key->'.$key);
+        $json = read();
+        print_r($json[$key]);
+        return $json[$key];
+    }
+    /*
+    **  Function to read the information contained in the configuration file
+    **  @return -> configuration file as json
+    */
+    function read() {
+        $this->$logger->info('Call to function read');
+        $file = "config.json";
+        if (is_file($file)) {
+            $this->$logger->info('File found.');
+            $json = json_decode(file_get_contents($file), true);
+            return $json;
+        } else {
+            $this->$logger->warning('File config.json doesn\'t exist.');
+            $this->$logger->info('Running file initialization.');
+            $this->initialize('read');
+        }
+    }
+    /*
+    **  Function to create the configuration file with default settings
+    **  @param? $callback -> (optional) callback to preceding function
+    **  @return -> none
+    */
+    function initialize($callback) {
+        $this->$logger->info('Created configuration file with name "config.json".');
+        $this->$logger->info('Started configuration file with value "directory": "downloads"');
+        $this->$logger->info('Started configuration file with value "flags": "[]"');
+        file_put_contents("config.json", "{\"directory\":\"downloads\",\"flags\":[]}");
+        $this->$callback();
     }
 }
-
-function initialize($callback){
-    //$logger->info('Call to function initialize');
-    // create configuration file with default settings
-    file_put_contents("config.file", "{\"directory\":\"downloads\",\"flags\":[]}");
-    $callback();
-}
+$cacher = new Cacher();
+$cacher->read();
--- logger.php
+++ logger.php
@@ -1,28 +1,28 @@
 <?php
 class Logger {
     private $file = "script.log";
-
+    private $channel = "default";
+    public function __construct($ch){
+        $this->$channel = $ch;
+    }
     function info($strng){
         $time = date('Y-m-d H:i:s');
-        $log = sprintf("%s INFO: %s".PHP_EOL, $time, $strng);
+        $log = sprintf("%s INFO: (%s) %s".PHP_EOL, $time, $this->$channel, $strng);
         file_put_contents($this->file, $log, FILE_APPEND|LOCK_EX);
     }
-
     function warning($strng){
         $time = date('Y-m-d H:i:s');
-        $log = sprintf("%s WARNING: %s".PHP_EOL, $time, $strng);
+        $log = sprintf("%s WARNING: (%s) %s".PHP_EOL, $time, $this->$channel, $strng);
         file_put_contents($this->file, $log, FILE_APPEND|LOCK_EX);
     }
-
     function error($strng, $e){
         $time = date('Y-m-d H:i:s');
-        $log = sprintf("%s ERROR: %s EXCEPTION: %s".PHP_EOL, $time, $strng, $e);
+        $log = sprintf("%s ERROR: (%s) %s EXCEPTION: %s".PHP_EOL, $time, $this->$channel, $strng, $e);
         file_put_contents($this->file, $log, FILE_APPEND|LOCK_EX);
     }
-
     function alert($strng){
         $time = date('Y-m-d H:i:s');
-        $log = sprintf("%s ALERT: %s".PHP_EOL, $time, $strng);
+        $log = sprintf("%s ALERT: (%s) %s".PHP_EOL, $time, $this->$channel, $strng);
         file_put_contents($this->file, $log, FILE_APPEND|LOCK_EX);
     }
 }
