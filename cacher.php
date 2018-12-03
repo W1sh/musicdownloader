d4ba4fc9f1f7ca70debc696963c3d6dbea9129e7
W1sh <brunoferreira.ebm@gmail.com>
2018-12-03 14:32:18 +0000

    created Logger class

--- cacher.php
+++ cacher.php
@@ -1,14 +1,16 @@
 <?php
+$logger = new Logger();
 
 function store($key, $data){
-    $logger->info('Call to function store with $key->'.$key.' and $data->'.$data);
+    //$logger->info('Call to function store with $key->'.$key.' and $data->'.$data);
     // store information in file
     $json = read();
     $json[$key] = $data;
-    file_put_contents($file, json_encode($json), FILE_APPEND|LOCK_EX);
+    file_put_contents($file, json_encode($json));
 }
 
 function fetch($key){
+    //$logger->info('Call to function retrieve with $key->'.$key);
     // retrieve information from file
     $json = read();
     print_r($json[$key]);
@@ -16,6 +18,7 @@ function fetch($key){
 }
 
 function read(){
+    //$logger->info('Call to function read');
     // read the configuration file
     $file = "config.json";
     if(is_file($file)){
@@ -27,6 +30,7 @@ function read(){
 }
 
 function initialize($callback){
+    //$logger->info('Call to function initialize');
     // create configuration file with default settings
     file_put_contents("config.file", "{\"directory\":\"downloads\",\"flags\":[]}");
     $callback();
