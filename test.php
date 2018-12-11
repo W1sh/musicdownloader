06eff983b64985e6b315aadb0dd35ff8414baac7
Bruno Ferreira <brunoferreira.ebm@gmail.com>
2018-12-11 09:49:19 +0000

    Delete test.php

--- test.php
+++
@@ -1,9 +0,0 @@
-<?php 
-include 'vendor/madcodez/youtube-downloader/src/YTDownloader.php';
-$yt = new YTDownloader();
-
-$links = $yt->getDownloadLinks("https://www.youtube.com/watch?v=HOSzLGrfdsM");
-
-echo isset($links);
-echo $links === false;
-print_r($links);
